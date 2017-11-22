a0ea7c4269529163cb537c49403300c9b0b1407e
Doug Rutnarak <dougrut1234@aol.com>
2017-11-22 03:02:17 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'shop/index'
+
+  get 'contact/index'
+
+  get 'faq/index'
+
+  get 'home/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
