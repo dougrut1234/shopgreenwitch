602e4bbfc3dbbce75c8a9ee3501b050831508714
Doug Rutnarak <dougrut1234@aol.com>
2017-12-01 14:27:20 -0500

    removed configs

--- config/application.rb
+++ config/application.rb
@@ -7,17 +7,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Shopgreenwitch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-
-configure(:development){set :database, "sqlite3:blog.sqlite3"}
-
 end
