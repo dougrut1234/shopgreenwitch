3acaab44a072dd47b1f5614e0e85c1713461db47
Andrew Park <apark@movableink.com>
2017-12-17 21:20:55 -0500

    changes env variables for mailchimp

--- app/jobs/subscribe_user_to_mailing_list_job.rb
+++ app/jobs/subscribe_user_to_mailing_list_job.rb
@@ -3,7 +3,7 @@ class SubscribeUserToMailingListJob < ApplicationJob
 
   def perform(email)
     gibbon = Gibbon::Request.new
-    gibbon.lists(ENV["MC_LIST"]).members.create({
+    gibbon.lists(Rails.application.secrets.mailchimp_list_id).members.create({
       body: { email_address: email, status: "subscribed" }
     })
   end
