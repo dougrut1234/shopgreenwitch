f432db9037061bc962143a792878fb3ec76ce0ee
Andrew Park <apark@movableink.com>
2018-04-22 15:12:49 -0400

    adds configuration for allowing emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,15 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'shopgreenwitch.com',
+    user_name:            'leila@shopgreenwitch.com',
+    password:             '<password>',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
