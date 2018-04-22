0eb288c6a8e9f13e60a0b7157555b4e26b650bae
Andrew Park <apark@movableink.com>
2018-04-22 14:16:06 -0400

    uncomments contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,25 @@
-# class ContactsController < ApplicationController
-# def new
-#     @contact = Contact.new
-#   end
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
 
-# def create
-#     @contact = Contact.new(params[:contact])
-#     @contact.request = request
-#     if @contact.deliver
-#       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
-#     else
-#       flash.now[:error] = 'Cannot send message.'
-#       render :new
-#     end
-#   end
-# end
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
 
-#   private
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+  end
 
-#   def contact_params
-#     params.require(:contact).permit(:name, :email, :message, :nickname)
-#   end
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message, :nickname)
+  end
+end
+
 
